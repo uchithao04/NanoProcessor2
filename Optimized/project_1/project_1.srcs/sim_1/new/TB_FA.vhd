f49464e9c7318986afbc082e2f97264831e5591d
NethmiNadeeshani <nethmi.24@cse.mrt.ac.lk>
2026-05-02 03:00:27 +0530

    optimized version testbench files added

---
+++ Optimized/project_1/project_1.srcs/sim_1/new/TB_FA.vhd
@@ -0,0 +1,116 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01.04.2026 20:00:00
+-- Design Name: 
+-- Module Name: TB_FA - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------------
+-- Group Members:
+-- GunawardenaNNDeA --- 240205A --- 111010101001001101 --- Last 4 bits: 1101
+-- GunasekaraPSI --- 240197X --- 111010101001000101 --- Last 4 bits: 0101
+-- GunarathnaIKL --- 240195N --- 111010101001000011 --- Last 4 bits: 0011
+-- HettiarachchiUO --- 240230U --- 111010101001100110 --- Last 4 bits: 0110
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_FA is
+--  Port ( );
+end TB_FA;
+
+architecture Behavioral of TB_FA is
+
+Component FA
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           C_in : in STD_LOGIC;
+           S : out STD_LOGIC;
+           C_out : out STD_LOGIC);
+End Component;
+
+Signal A, B, C_in, S, C_out : STD_LOGIC;
+
+begin
+
+    UUT: FA Port Map(
+        A => A,
+        B => B,
+        C_in => C_in,
+        S => S,
+        C_out => C_out
+    );
+
+Process Begin
+
+    -- GunawardenaNNDeA (1101)
+    A <= '0';
+    B <= '0';
+    C_in <= '0';
+    wait for 80ns;
+
+    -- GunasekaraPSI (0101)
+    A <= '0';
+    B <= '0';
+    C_in <= '1';
+    wait for 80ns;
+
+    -- GunarathnaIKL (0011)
+    A <= '0';
+    B <= '1';
+    C_in <= '0';
+    wait for 80ns;
+
+    -- HettiarachchiUO (0110)
+    A <= '0';
+    B <= '1';
+    C_in <= '1';
+    wait for 80ns;
+
+    -- Full truth table continued
+    A <= '1';
+    B <= '0';
+    C_in <= '0';
+    wait for 80ns;
+
+    A <= '1';
+    B <= '0';
+    C_in <= '1';
+    wait for 80ns;
+
+    A <= '1';
+    B <= '1';
+    C_in <= '0';
+    wait for 80ns;
+
+    A <= '1';
+    B <= '1';
+    C_in <= '1';
+    wait;
+
+End Process;
+
+end Behavioral;
